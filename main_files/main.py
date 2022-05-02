07658cedfbc6fc60688ec2b0b499f0b1383e5f5d
JasonEide <jeide15@yahoo.com>
2022-05-02 16:33:08 -0400

    Minor fixes

--- main_files/main.py
+++ main_files/main.py
@@ -1,6 +1,7 @@
 from twitch_listener import listener
 import counter
 import secrets
+import os
 
 # Connect to Twitch
 bot = listener.connect_twitch(secrets.id, secrets.oAuth, secrets.clientID)
@@ -9,9 +10,13 @@ bot = listener.connect_twitch(secrets.id, secrets.oAuth, secrets.clientID)
 channels_to_listen_to = ['HasanAbi']
 
 # Scrape live chat data into raw log files. (Duration is seconds)
-bot.listen(channels_to_listen_to, duration = 5)
+bot.listen(channels_to_listen_to, duration=5)
 
 # Convert log files into .CSV format
-bot.parse_logs(timestamp = True)
+bot.parse_logs(timestamp=True)
 
 print(counter.foo(channels_to_listen_to, 'poggers'))
+
+for i in channels_to_listen_to:
+    os.remove(f'{i}.log')
+    os.remove(f'{i}.csv')
