07d4719db4a5c4c99932e53067569fafd9424569
JasonEide <jeide15@yahoo.com>
2022-05-02 20:48:04 -0400

    Minor additions

--- main_files/main.py
+++ main_files/main.py
@@ -4,13 +4,18 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+print('Insert twitch streamers:')
+twitch_streamers = input()
+print('How long would you like to listen to chat?')
+duration = int(input())
+
 # Connect to Twitch
 bot = listener.connect_twitch(os.environ['ID'], os.environ['OAUTH'], os.environ['CLIENTID'])
 
 # List of channels to connect to
-channels_to_listen_to = ['HasanAbi']
+channels_to_listen_to = [twitch_streamers]
 
 # Scrape live chat data into raw log files. (Duration is seconds)
-bot.listen(channels_to_listen_to, duration=5)
+bot.listen(channels_to_listen_to, duration)
 
 print(counter.foo(channels_to_listen_to, 'poggers'))
